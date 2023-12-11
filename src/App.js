d6378adf8c79d483fdf132deae2d507dd1eb50dd
hansol <thf0128@gmail.com>
2023-12-11 11:01:07 +0900

    SNSBOARD 프론트엔드 # 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import './App.css';
 import MainPage from './Component/MainPage';
+import SnsBoard from './Component/SnsBoard';
+import MyDetail from './Component/MyDetail';
+import UserDetail from './Component/UserDetail';
+import UserDetail2 from './Component/UserDetail2';
+import UserDetail3 from './Component/UserDetail3';
+import UserDetail4 from './Component/UserDetail4';
+import Setting from './Component/Setting';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ProfileEdit from './Component/ProfileEdit';
+import Comment from './Component/Comment';
 
 function App() {
   return (
     <>
       <div>
-        <MainPage />
+        <Router>
+          <Routes>
+            <Route
+              path='/'
+              element={<SnsBoard />}
+            />
+            <Route
+              path='/UserDetail'
+              element={<UserDetail />}
+            />
+            <Route
+              path='/Setting'
+              element={<Setting />}
+            />
+            <Route
+              path='/Comment'
+              element={<Comment />}
+            />
+            <Route
+              path='/ProfileEdit'
+              element={<ProfileEdit />}
+            />
+            <Route
+              path='/MyDetail'
+              element={<MyDetail />}
+            />
+          </Routes>
+        </Router>
       </div>
     </>
   );
---
+++ src/Component/Comment.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import './Comment.css';
+import { Link } from 'react-router-dom';
+
+const Comment = () => {
+  return (
+    <div className='comment'>
+      <div className='div'>
+        <div className='side-bar'>
+          <div className='sidebar-content'>
+            <div className='logout'>
+              <div className='text-wrapper'>Logout</div>
+            </div>
+            <div className='my-page'>
+              <div className='my-page-text'>Mypage</div>
+            </div>
+            <div className='BOARD'>
+              <div className='text-wrapper'>BOARD</div>
+            </div>
+            <div className='CHAT'>
+              <div className='text-wrapper-2'>CHAT</div>
+            </div>
+            <div className='RECIPE'>
+              <div className='text-wrapper-3'>RECIPE</div>
+            </div>
+            <div className='HOTPLACE'>
+              <div className='text-wrapper-4'>HOTPLACE</div>
+            </div>
+          </div>
+          <div className='logo'>
+            <div className='text-wrapper-5'>HONBAM</div>
+          </div>
+        </div>
+        <div className='overlap-group-wrapper'>
+          <div className='overlap-group'>
+            <div className='rectangle' />
+            <div className='vector' />
+            <div className='img' />
+            <div className='vector-2' />
+
+            <div className='intro'>
+              <div className='text-wrapper-6'>bmabam1234</div>
+            </div>
+            <div className='div-wrapper'>
+              <p className='text-wrapper-7'>많은 댓글 부탁드려요</p>
+            </div>
+            <div className='intro-2'>
+              <div className='text-wrapper-8'>1일</div>
+            </div>
+            <div className='id'>
+              <div className='text-wrapper-9'>댓글</div>
+            </div>
+
+            <div className='image' />
+            <div className='intro-3'>
+              <div className='text-wrapper-10'>abc1234</div>
+            </div>
+            <div className='intro-4'>
+              <div className='text-wrapper-7'>게시글 잘보고 가요</div>
+            </div>
+            <div className='intro-5'>
+              <div className='text-wrapper-8'>3일</div>
+            </div>
+            <div className='nature-person-girl' />
+            <div className='commentTxt' />
+            <div className='vector-3' />
+            <div className='intro-6'>
+              <div className='text-wrapper-10'>abc1234</div>
+            </div>
+            <div className='intro-7'>
+              <div className='text-wrapper-7'>즐거운 혼술되세요</div>
+            </div>
+            <div className='intro-8'>
+              <div className='text-wrapper-8'>5일</div>
+            </div>
+            <div className='person' />
+            <div className='comment1' />
+            <input
+              className='intro-9'
+              type='text'
+              placeholder='댓글 추가...'
+            ></input>
+            <div className='vector-4' />
+          </div>
+          <div className='back'>
+            <Link to='/'>
+              <button
+                type='button'
+                className='backBtn1'
+              ></button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Comment;
