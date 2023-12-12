356f4afa85cbf06c68a89a86b0fdb80fa890160f
minji <gkstnals2626@naver.com>
2023-12-12 15:27:57 +0900

    hotplace

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Navbar from './Component/Navbar/Navbar';
+import SearchPage from './Component/SearchPage/SearchPage';
 
 function App() {
   return (
     <>
       <div>
         <Navbar />
+        <div>
+          <SearchPage />
+        </div>
       </div>
     </>
   );
